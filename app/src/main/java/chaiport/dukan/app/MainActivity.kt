3b8276c4303d80a0f9681e73813b85223dd51b9e
aagam-chai <90909298+aagam-chai@users.noreply.github.com>
2023-03-23 17:00:30 +0530

    Updated with the latest version

--- app/src/main/java/chaiport/dukan/app/MainActivity.kt
+++ app/src/main/java/chaiport/dukan/app/MainActivity.kt
@@ -3,17 +3,18 @@ package chaiport.dukan.app
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Observer
 import chaiport.dukan.app.RequestBodies.getTokenizationRequest
 import chaiport.dukan.app.RequestBodies.getWebCheckoutRequest
 import chaiport.dukan.app.RequestBodies.getWithoutTokenizationRequest
-import com.android.chaipaylibrary.ChaiPort
-import com.android.chaipaylibrary.ChaiPortImpl
+import com.android.chaipaylibrary.PortOne
+import com.android.chaipaylibrary.PortOneImpl
 import com.android.chaipaylibrary.constants.PAYMENT_STATUS
 import com.android.chaipaylibrary.constants.PAYMENT_STATUS_REQUEST_CODE
 import com.android.chaipaylibrary.constants.PAYOUT_REQUEST_CODE
 import com.android.chaipaylibrary.constants.RESULT_CODE
 import com.android.chaipaylibrary.dto.PaymentDto
+import com.android.chaipaylibrary.networkCalls.ApiCallbackInterface
+import com.android.chaipaylibrary.utils.LoggerUtil
 import com.google.gson.Gson
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -23,9 +24,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val chaiPort: ChaiPort
+        val chaiPort: PortOne
 
-        chaiPort = ChaiPortImpl(
+        chaiPort = PortOneImpl(
             context = this,
             environment = RequestBodies.environment,
             devEnvironment = RequestBodies.devEnvironment
@@ -36,12 +37,41 @@ class MainActivity : AppCompatActivity() {
             chaiPort.updatePaymentStatus(paymentStatus)
         }
 
-        val paymentStatusObserver =
-            Observer<PaymentDto.CheckoutUsingCreditCardResponse> { paymentStatus ->
-                tvPaymentStatusValue.text = paymentStatus.toString()
-            }
+        chaiPort.getBankList(
+            "GBPRIMEPAY",
+            RequestBodies.getBankListRequest(),
+            object : ApiCallbackInterface<PaymentDto.BankListResponse> {
+                override fun onSucceed(response: PaymentDto.BankListResponse) {
+                    LoggerUtil.info("Successful")
+                }
 
-        chaiPort.showPaymentStatus().observe(this, paymentStatusObserver)
+                override fun onFailure(
+                    errorCode: Int?,
+                    status: String?,
+                    errorMessage: String,
+                    throwable: Throwable
+                ) {
+                    LoggerUtil.info("Failed")
+                }
+
+            })
+
+        chaiPort.getDBTDetails(RequestBodies.clientKey,
+            object : ApiCallbackInterface<PaymentDto.DBTResponse> {
+                override fun onSucceed(response: PaymentDto.DBTResponse) {
+                    LoggerUtil.info("Successful")
+                }
+
+                override fun onFailure(
+                    errorCode: Int?,
+                    status: String?,
+                    errorMessage: String,
+                    throwable: Throwable
+                ) {
+                    LoggerUtil.info("Failed")
+                }
+
+            })
 
         buttonWebCheckout.setOnClickListener {
             chaiPort.checkoutUsingWeb(
@@ -54,7 +84,7 @@ class MainActivity : AppCompatActivity() {
             chaiPort.checkoutWithoutTokenization(request = getWithoutTokenizationRequest())
         }
         buttonTokenization.setOnClickListener {
-            chaiPort.initiatePaymentWithTokenization(request = getTokenizationRequest())
+            chaiPort.checkoutWithTokenization(request = getTokenizationRequest())
         }
     }
 
@@ -67,8 +97,8 @@ class MainActivity : AppCompatActivity() {
                     val paymentStatus: PaymentDto.PaymentStatus? =
                         (data.getSerializableExtra(PAYMENT_STATUS)
                             ?: "Empty") as PaymentDto.PaymentStatus
-                    val gson = Gson()
-                    val json = gson.toJson(paymentStatus)
+                    val gson= Gson()
+                    val json=gson.toJson(paymentStatus)
                     tvPaymentStatusValue.text = json
                 }
 
